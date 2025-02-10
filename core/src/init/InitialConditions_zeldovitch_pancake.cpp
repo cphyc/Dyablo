b487627b10a4aca4808a4cba2bba789e48addd98
daubert <dominique.aubert@astro.unistra.fr>
2025-02-10 11:51:20 +0100

    Re-Implemented and checked self-sufficient Zeldovich. Valid for abitrary cosmology.

--- core/src/init/InitialConditions_zeldovitch_pancake.cpp
+++ core/src/init/InitialConditions_zeldovitch_pancake.cpp
@@ -14,6 +14,7 @@ struct AnalyticalFormula_Zeldovitch_pancake : public AnalyticalFormula_base{
   const real_t xmin, xmax;
   real_t omegab;
   real_t omegam;
+  real_t omegav;
   real_t vmax;
   real_t fact;
 
@@ -26,18 +27,68 @@ struct AnalyticalFormula_Zeldovitch_pancake : public AnalyticalFormula_base{
     error_max(configMap.getValue<real_t>("amr", "error_max", 0.8)),
     xmin( configMap.getValue<real_t>("mesh","xmin", 0) ),
     xmax( configMap.getValue<real_t>("mesh","xmax", 1) ),
-    omegab(configMap.getValue<real_t>("cosmology","omegab", 0.999)),
-    omegam(configMap.getValue<real_t>("cosmology","omegam", 1.0))
+    omegab(configMap.getValue<real_t>("cosmology","omegab", 0.049)),
+    omegam(configMap.getValue<real_t>("cosmology","omegam", 0.3)),
+    omegav(configMap.getValue<real_t>("cosmology","omegav", 0.7))
   {
     DYABLO_ASSERT_HOST_RELEASE(ndim == 3, "Initial conditions only for 3D");
 
-    real_t a_cross = configMap.getValue<real_t>("zeldovitch_pancake", "aCross", 0.1);
-    real_t a_start = configMap.getValue<real_t>("cosmology", "aStart", 0.01);
+    real_t across = configMap.getValue<real_t>("zeldovitch_pancake", "aCross", 0.1);
+    real_t astart = configMap.getValue<real_t>("cosmology", "aStart", 0.01);
+      
+    real_t omegak = 1.0 - omegam - omegav;
+    real_t etastart    = sqrt(omegam / astart + omegav * astart * astart + omegak);
+    real_t etacross    = sqrt(omegam / across + omegav * across * across + omegak);
 
-    real_t zc = 1/a_cross - 1;
-    real_t zi = 1/a_start - 1;
-    this->vmax = (1/(M_PI)) * (1+zc)/std::pow(1+zi, 1.5);
-    this->fact = (1+zc)/(1+zi);
+    real_t dladt = astart * etastart;
+
+    real_t fomega;
+    real_t dcross;
+    real_t dplus;
+
+    if (omegam >= 1.0 && omegav <= 0.0)
+      fomega = 1.0;
+    else
+    {
+      {
+        auto ddplus = [&](real_t a)
+        {
+          if (a <= 0.0)
+            return 0.0;
+
+          real_t eta = sqrt(omegam / a + omegav * a * a + 1.0 - omegam - omegav);
+          return 2.5 / (eta * eta * eta);
+        };
+
+        //------------------------------ DPLUS
+        // UGLY trapezoid rule integration
+        const real_t Np = 10000;
+        const real_t da = astart / Np;
+        real_t sum      = 0.0;
+        for (int i = 0; i < Np; ++i)
+        {
+          sum += 0.5 * (ddplus(i * da) + ddplus((i + 1) * da));
+        }
+        sum *= da;
+
+        dplus = etastart / astart * sum;
+
+        //------------------------------ DCROSS
+        const real_t db = across / Np;
+        sum      = 0.0;
+        for (int i = 0; i < Np; ++i)
+        {
+          sum += 0.5 * (ddplus(i * db) + ddplus((i + 1) * db));
+        }
+        sum *= db;
+
+        dcross = etacross / across * sum;
+      }
+      fomega = (2.5 / dplus - 1.5 * omegam / astart - omegak) / (etastart * etastart);
+    }
+    this->fact = dplus / dcross;
+    this->vmax = dplus / dcross * fomega * dladt / sqrt(omegam) / (M_PI); 
+  
   }
 
 
@@ -56,21 +107,15 @@ struct AnalyticalFormula_Zeldovitch_pancake : public AnalyticalFormula_base{
   ConsHydroState value( real_t x, real_t y, real_t z, real_t dx, real_t dy, real_t dz ) const
   {
     PrimHydroState res {};
-
-    res.rho = (this->omegab/this->omegam) * (1 - fact*cos(2*M_PI*x));  // equation 105 bryan et al  // ressayer la formule ramses
-    //res.rho = (this->omegab/this->omegam) * (1 + fact*cos(2*M_PI*x)); //equation 28 ramses elle marche moins bien
+    res.rho = (this->omegab/this->omegam) / (1.0 + fact*cos( z * 2 * M_PI));
     res.p = this->smallp;
 
-    // 0 at edges and center of box,
-    // velocity vmax towards center at 1/4, 3/4 box
-    // https://arxiv.org/abs/astro-ph/0111367
-    // u = -1/2pi * (1+zc)/(1+zi)^3/2 * sin( 2pi x )     (22)
-    res.u   = vmax * sin( x * 2 * M_PI );
+    res.u   = 0.0;
     res.v   = 0.0;
-    res.w   = 0.0;
+    res.w   = vmax * sin( z * 2 * M_PI );
 
     ConsHydroState cons_res = primToCons<3>(res, gamma0);
-    return cons_res; 
+    return cons_res;
   }
 };
 } // namespace dyablo
