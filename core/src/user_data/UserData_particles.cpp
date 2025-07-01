b662e82e06db1f9d2780ed2d2d870f17fbfed65a
Arnaud Durocher <arnaud.durocher@cea.fr>
2025-07-01 19:34:01 +0200

    Add UserData::merge_particles_if

--- core/src/user_data/UserData_particles.cpp
+++ core/src/user_data/UserData_particles.cpp
@@ -112,6 +112,16 @@ public:
     {
         attribute_index.erase( attribute_name );
     }
+    void resize( uint32_t new_num_particles )
+    {
+        uint32_t old_size = particles.getNumParticles();
+        if (new_num_particles == old_size) return;
+
+        size_t Nprops = particles.particle_data.extent(1);
+        Kokkos::resize(particles.particle_data, new_num_particles, Nprops);
+        size_t Ndim = particles.particle_position.extent(1);
+        Kokkos::resize(particles.particle_position, new_num_particles, Ndim);
+    }
     void distributeParticles()
     {
         ViewCommunicator part_comm = foreach_particle.get_distribute_communicator( particles );
@@ -166,7 +176,13 @@ public:
             res.insert( p.first );
         }
         return res;
-    } 
+    }
+
+    void delete_ParticleArray( const std::string& name )
+    {
+        DYABLO_ASSERT_HOST_RELEASE( this->has_ParticleArray(name), "UserData_particles::delete_ParticleArray() - particle array does not exist : " << name );
+        particle_containers.erase( name );
+    }
 
 //private:
     ForeachParticle foreach_particle;
@@ -260,6 +276,79 @@ void UserData::distributeAllParticles()
 {
   for(auto& pair : this->particles.pdata->particle_containers)
     pair.second.distributeParticles();
+}
+
+void UserData::delete_ParticleArray( const std::string& name )
+{
+  this->particles.pdata->delete_ParticleArray( name );
+}
+
+void UserData::merge_particles_if( const std::string& id_dest, const std::string& id_to_merge, const std::string& mask_field )
+{
+    auto& pdata = *this->particles.pdata;
+    DYABLO_ASSERT_HOST_RELEASE( this->has_ParticleArray(id_dest), "merge_particles_if error : destination array '"<<id_dest<<"' does not exist" );
+    DYABLO_ASSERT_HOST_RELEASE( this->has_ParticleArray(id_to_merge), "merge_particles_if error : array to merge '"<<id_to_merge<<"' does not exist" );
+    DYABLO_ASSERT_HOST_RELEASE( this->has_ParticleAttribute(id_to_merge, mask_field), "merge_particles_if error : mask field '"<<id_to_merge<<"/"<<mask_field<<"' does not exist" );
+
+    std::vector<ParticleAccessor::AttributeInfo> all_attr;
+    VarIndex Imask = -1;
+    {
+        for( const std::string& attr : this->getEnabledParticleAttributes( id_dest ) )
+        {
+            VarIndex ivar = all_attr.size();
+            all_attr.push_back({attr, ivar});
+            if( attr == mask_field )
+            {
+                Imask = ivar;
+            }
+        }
+    }
+    int nb_attr = all_attr.size();
+
+    int n_to_merge = 0;
+    {
+        auto Pto_merge = this->getParticleArray(id_to_merge);
+        auto Pmask = this->getParticleAccessor(id_to_merge, {{mask_field, 0}});
+        pdata.foreach_particle.reduce_particle("count_to_merge", Pto_merge,
+            KOKKOS_LAMBDA(const ForeachParticle::ParticleIndex& iPart, int& count)
+        {
+            if( Pmask.at(iPart, 0) != 0.0 )
+                count++;
+        }, n_to_merge);
+    }
+
+    auto& Pout_container = pdata.getParticleContainer( id_dest );
+    uint32_t Pout_initial_size = Pout_container.getParticleArray().getNumParticles();
+    Pout_container.resize( Pout_initial_size + n_to_merge );
+
+    {
+        auto Pin = this->getParticleArray(id_to_merge);
+        auto Pin_data = this->getParticleAccessor( id_to_merge, all_attr );
+
+        auto Pout = this->getParticleArray(id_dest);
+        auto Pout_data = this->getParticleAccessor( id_dest, all_attr );
+
+        Kokkos::View<uint32_t> count("count");
+        pdata.foreach_particle.foreach_particle( "merge_copy", Pin,
+            KOKKOS_LAMBDA(const ForeachParticle::ParticleIndex& iPart_in)
+        {
+            if( Pin_data.at_ivar(iPart_in, Imask) != 0 )
+            {
+                ForeachParticle::ParticleIndex iPart_out = Pout_initial_size + Kokkos::atomic_fetch_add(&count(), 1);
+
+                Pout.pos( iPart_out, IX ) = Pin.pos( iPart_in, IX );
+                Pout.pos( iPart_out, IY ) = Pin.pos( iPart_in, IY );
+                Pout.pos( iPart_out, IZ ) = Pin.pos( iPart_in, IZ );
+
+                for( int i=0; i<nb_attr; i++ )
+                {
+                    Pout_data.at_ivar(iPart_out, i) = Pin_data.at_ivar(iPart_in, i);
+                }
+            }
+        });
+    }
+
+    this->delete_ParticleArray(id_to_merge);
 }
 
 UserData::ParticleAccessor UserData::getParticleAccessor( const std::string& array_name, const std::vector<ParticleAccessor_AttributeInfo>& attribute_info ) const
