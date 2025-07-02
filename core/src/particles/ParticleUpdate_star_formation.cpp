47c30663f4c21f9b96606f1a44c563336f3dc04a
Arnaud Durocher <arnaud.durocher@cea.fr>
2025-07-02 11:23:36 +0200

    Remove ugly ternary for boolean

--- core/src/particles/ParticleUpdate_star_formation.cpp
+++ core/src/particles/ParticleUpdate_star_formation.cpp
@@ -135,7 +135,7 @@ public:
       auto u = policy.getConsState( Uin, iCell );
       auto q = policy.consToPrim( u );
 
-      bool starForming = isStarFormingCell(q.rho, q.p) ? 1 : 0;
+      bool starForming = isStarFormingCell(q.rho, q.p);
       count += starForming ? 1 : 0;
     }, n_star_forming_cells);
 
@@ -149,7 +149,7 @@ public:
       auto u = policy.getConsState( Uin, iCell );
       auto q = policy.consToPrim( u );
 
-      bool starForming = isStarFormingCell(q.rho, q.p) ? 1 : 0;
+      bool starForming = isStarFormingCell(q.rho, q.p);
       
       if( starForming )
       {
