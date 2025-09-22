359d79143f8204d28dc183f2abdc58e9a4471b5c
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-09-22 12:28:40 +0200

    Fix run_and_verify_all.sh

--- .gitlab-ci/run_and_verify_all.sh
+++ .gitlab-ci/run_and_verify_all.sh
@@ -46,10 +46,10 @@ run_and_verify(){
 
 run_and_verify test_sod_2D.ini "validate_sod.py test_sod_2D_main.xmf 1e-2 ../../../sod_2D/sod.png" sod_2D 
 
-(
-    cd build/dyablo/bin
-    python3 ../../../settings/cosmo/zeldovitch_generate_grafic.py
-)
+
+cd build/dyablo/bin
+python3 ../../../settings/cosmo/zeldovitch_generate_grafic.py
+cd ../../..
 
 run_and_verify test_zeldovitch_grafic.ini "../../../settings/cosmo/validate_zeldovitch.py zeldovitch_main.xmf 0.2 ../../../zeldovitch_grafic/zeldovitch_grafic.png" zeldovitch_grafic 
 
@@ -60,10 +60,9 @@ run_and_verify beam.ini "../../../settings/cosmo/validate_beam.py beam_main.xmf 
 run_and_verify test_star_formation.ini "validate_star_formation.py star_formation_main.xmf 1e-2 ../../../star_formation/star_formation.png" star_formation
 
 # Create cooling table
-(
-    cd build/dyablo/bin
-    python3 ../../../settings/cooling/create_analytical_cooling_table.py
-)
+cd build/dyablo/bin
+python3 ../../../settings/cooling/create_analytical_cooling_table.py
+cd ../../..
 
 run_and_verify test_cooling.ini "validate_cooling.py cooling_main.xmf 0.05 ../../../cooling/cooling.png" cooling
 
