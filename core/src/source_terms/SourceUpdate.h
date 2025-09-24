bdaa1531e9ef7a3cf70f18ed05da6565afa346c6
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-09-24 11:08:36 +0200

    Move to using policies

--- core/src/source_terms/SourceUpdate.h
+++ core/src/source_terms/SourceUpdate.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "source_terms/SourceUpdate_base.h"
+#include "hyperbolic/policy/HyperbolicPolicy_Hydro.h"
+#include "hyperbolic/policy/HyperbolicPolicy_GLMMHD.h"
 
 template<>
 inline bool dyablo::SourceUpdateFactory::init()
