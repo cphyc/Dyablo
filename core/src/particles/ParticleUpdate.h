04430d106df25a548838a0390db7e6620630b678
Anatole Storck <storckanatole@gmail.com>
2026-03-12 14:18:07 +0100

    add radiative feedback

--- core/src/particles/ParticleUpdate.h
+++ core/src/particles/ParticleUpdate.h
@@ -15,6 +15,7 @@ class ParticleUpdate_TSC_density;
 class ParticleUpdate_star_formation;
 
 class ParticleUpdate_feedback;
+class ParticleUpdate_radiative_feedback;
 
 } //namespace dyablo 
 
@@ -32,6 +33,8 @@ inline bool dyablo::ParticleUpdateFactory::init()
   DECLARE_REGISTERED(dyablo::ParticleUpdate_star_formation);
 
   DECLARE_REGISTERED(dyablo::ParticleUpdate_feedback);
+
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_radiative_feedback);
 
   return true;
 }
---
+++ core/src/particles/ParticleUpdate_radiative_feedback.cpp
@@ -0,0 +1,107 @@
+#include "ParticleUpdate_base.h"
+#include "utils/units/Units.h"
+#include "ForeachParticle.h"
+#include "states/State_hydro.h"
+
+#include <Kokkos_Core.hpp>
+
+namespace dyablo {
+
+class ParticleUpdate_radiative_feedback : public ParticleUpdate {
+public:
+  using pos_t = Kokkos::Array<real_t, 3>;
+
+  ParticleUpdate_radiative_feedback(
+    ConfigMap& configMap,
+    ForeachCell& foreach_cell,
+    Timers& timers)
+  : foreach_cell    ( foreach_cell ),
+    foreach_particle( foreach_cell.get_amr_mesh(), configMap ),
+    timers          ( timers ),
+    photon_rate     ( configMap.getValue<real_t>("star_feedback", "photon_rate", 1e49) ),
+    cosmology       ( configMap.getValue<bool>("cosmology", "active", false) )
+  {
+  }
+
+  ~ParticleUpdate_radiative_feedback() {}
+
+  void update(UserData& U, ScalarSimulationData& scalar_data)
+  {
+
+    const real_t t = cosmology ? scalar_data.get<real_t>("time_physical") : scalar_data.get<real_t>("time");
+    const real_t dt = scalar_data.get<real_t>("dt");
+
+    enum VarIndex_rt {
+      IE_rad, IFx_rad, IFy_rad, IFz_rad
+    };
+    enum VarIndex_particle {
+      IMASS, IBIRTH,
+    };
+
+    timers.get("ParticleUpdate_radiative_feedback").start();
+
+    std::vector<UserData_fields::FieldAccessor_FieldInfo>
+      Uout_infos = {{"e_rad", IE_rad},    {"fx_rad", IFx_rad},    {"fy_rad", IFy_rad},    {"fz_rad", IFz_rad}};
+    std::vector<UserData_particles::ParticleAccessor_AttributeInfo>
+      pinfos = {{"mass", IMASS}, {"birth_time", IBIRTH}};
+
+    // Get accessors
+    auto Ppos = U.getParticleArray( "particles" );
+    auto Pdata = U.getParticleAccessor( "particles", pinfos );
+    auto Uout = U.getAccessor( Uout_infos );
+
+    ForeachCell::CellMetaData cells = foreach_cell.getCellMetaData();
+
+    real_t aexp = scalar_data.get<real_t>("aexp");
+
+    // Gather SN feedback parameters
+    const real_t photon_rate = this->photon_rate;
+    const real_t dt_physical = Units::supercomoving_to_physical<Units::Time>(
+      (dt * Units::code_units().getUnit<Units::Time>()).convert_to(Units::s()),
+      aexp
+    );
+
+    foreach_particle.foreach_particle( "particles_update_feedback", Ppos,
+      KOKKOS_LAMBDA( const ForeachParticle::ParticleIndex& iPart )
+    {
+      // Age of the particle
+      real_t age_physical = t - Pdata.at(iPart, IBIRTH);
+
+      pos_t part_pos = {Ppos.pos(iPart, IX), Ppos.pos(iPart, IY), Ppos.pos(iPart, IZ)};
+
+      ForeachCell::CellIndex iCell = cells.getCellFromPos( part_pos );
+
+      pos_t cell_size = cells.getCellSize( iCell );
+      real_t cell_volume = cell_size[IX] * cell_size[IY] * cell_size[IZ];
+
+      const real_t cell_volume_physical = Units::supercomoving_to_physical<Units::Volume>(
+        (cell_volume * Units::code_units().getUnit<Units::Volume>()).convert_to(Units::cm3()),
+        aexp
+      );
+
+      // Compute ejecta mass, thermal energy + kinetic energy
+      real_t Mstar = Pdata.at(iPart, IMASS);
+
+      // Atomic are mandatory since multiple particles can explode in the same cell
+      Kokkos::atomic_add(&Uout.at(iCell, IE_rad), photon_rate * dt_physical / cell_volume_physical);
+
+    });
+
+    timers.get("ParticleUpdate_radiative_feedback").stop();
+  }
+
+private:
+  ForeachCell& foreach_cell;
+  ForeachParticle foreach_particle;
+  Timers& timers;
+
+  real_t photon_rate;
+
+  bool cosmology;
+};
+
+} // namespace dyablo
+
+FACTORY_REGISTER( dyablo::ParticleUpdateFactory,
+                  dyablo::ParticleUpdate_radiative_feedback,
+                  "ParticleUpdate_radiative_feedback")
