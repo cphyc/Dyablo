797580c4ab6f5f4ba620e008f09627ee5d54033d
Arnaud Durocher <arnaud.durocher@cea.fr>
2026-04-17 11:39:04 +0200

    Add passive scalars to RK2 hyperbolic updates

--- core/src/hyperbolic/GLMMHDUpdate_RK2.cpp
+++ core/src/hyperbolic/GLMMHDUpdate_RK2.cpp
@@ -1,4 +1,5 @@
 #include "hyperbolic/policy/HyperbolicPolicy_GLMMHD.h"
+#include "hyperbolic/policy/HyperbolicPolicy_passive_scalars.h"
 
 #include "hyperbolic/scheme/Hyperbolic_RK2.h"
 
@@ -11,8 +12,24 @@ public:
   using Hyperbolic_RK2<HyperbolicPolicy_GLMMHD>::Hyperbolic_RK2;
 };
 
+namespace {
+using HyperbolicPolicy_GLMMHD_2_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_GLMMHD_impl, 2> >;
+}
+
+class GLMMHDUpdate_RK2_2_passive_scalars 
+  : public Hyperbolic_RK2<HyperbolicPolicy_GLMMHD_2_passive_scalars>
+{
+  
+public:
+  using Hyperbolic_RK2<HyperbolicPolicy_GLMMHD_2_passive_scalars>::Hyperbolic_RK2;
+};
+
 } //namespace dyablo
 
 FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
                   dyablo::GLMMHDUpdate_RK2, 
                   "GLMMHDUpdate_RK2")
+
+FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
+                  dyablo::GLMMHDUpdate_RK2_2_passive_scalars, 
+                  "GLMMHDUpdate_RK2_2_passive_scalars")
--- core/src/hyperbolic/HydroUpdate_RK2.cpp
+++ core/src/hyperbolic/HydroUpdate_RK2.cpp
@@ -1,4 +1,5 @@
 #include "hyperbolic/policy/HyperbolicPolicy_Hydro.h"
+#include "hyperbolic/policy/HyperbolicPolicy_passive_scalars.h"
 
 #include "hyperbolic/scheme/Hyperbolic_RK2.h"
 
@@ -11,8 +12,23 @@ public:
   using Hyperbolic_RK2<HyperbolicPolicy_Hydro>::Hyperbolic_RK2;
 };
 
+namespace {
+  using HyperbolicPolicy_Hydro_2_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 2> >;
+}
+
+class HydroUpdate_RK2_2_passive_scalars 
+  : public Hyperbolic_RK2<HyperbolicPolicy_Hydro_2_passive_scalars>
+{
+public:
+  using Hyperbolic_RK2<HyperbolicPolicy_Hydro_2_passive_scalars>::Hyperbolic_RK2;
+};
+
 } //namespace dyablo
 
 FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
                   dyablo::HydroUpdate_RK2, 
                   "HydroUpdate_RK2")
+
+FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory,
+                  dyablo::HydroUpdate_RK2_2_passive_scalars,
+                  "HydroUpdate_RK2_2_passive_scalars")
