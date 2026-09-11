e92689e3fc788e67ba8b2bd5a59d594982c013ce
Arnaud Durocher <arnaud.durocher@cea.fr>
2026-09-11 16:30:34 +0200

    Remove ghost check for LightOctree::findNeighbor

--- core/src/amr/LightOctree_hashmap_precompute.h
+++ core/src/amr/LightOctree_hashmap_precompute.h
@@ -169,7 +169,8 @@ public:
         }
         if( !accepts_ghosts )
         {
-            DYABLO_ASSERT_KOKKOS_DEBUG(!iOct.isGhost, "LightOctree_hashmap_precompute::findNeighbor_aux : iOct is a ghost but ghosts are disabled");
+            // TODO : Enable this ghost check (watch out for issue #275)
+            //DYABLO_ASSERT_KOKKOS_DEBUG(!iOct.isGhost, "LightOctree_hashmap_precompute::findNeighbor_aux : iOct is a ghost but ghosts are disabled");
         }
 
         uint32_t iOct_local;
